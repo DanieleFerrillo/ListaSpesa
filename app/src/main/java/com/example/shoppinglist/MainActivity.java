89140750196694174180cf9ca3950cd9050161e4
DanieleFerrrillo <danyferrillo@gmail.com>
2020-04-01 12:52:43 +0200

    arrayList di prodotti

--- app/src/main/java/com/example/shoppinglist/MainActivity.java
+++ app/src/main/java/com/example/shoppinglist/MainActivity.java
@@ -4,11 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList<ProductItem> exampleList = new ArrayList<>();
+        exampleList.add(new ProductItem(R.drawable.ic_product_a, "Line 1","Line 2"));
+        exampleList.add(new ProductItem(R.drawable.ic_product_b, "Line 3","Line 4"));
+        exampleList.add(new ProductItem(R.drawable.ic_product_c, "Line 5","Line 6"));
+        exampleList.add(new ProductItem(R.drawable.ic_product_a, "Line 7","Line 8"));
+        exampleList.add(new ProductItem(R.drawable.ic_product_b, "Line 9","Line 10"));
+        exampleList.add(new ProductItem(R.drawable.ic_product_c, "Line 11","Line 12"));
     }
 }
