7ac3d4781657f39f46a5c47fc2d6bea6431baf51
DanieleFerrrillo <danyferrillo@gmail.com>
2020-04-01 14:22:22 +0200

    assegnazione adapter e layoutManger alla recyclerView

--- app/src/main/java/com/example/shoppinglist/MainActivity.java
+++ app/src/main/java/com/example/shoppinglist/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.shoppinglist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
@@ -8,7 +9,7 @@ import android.os.Bundle;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ArrayList<ProductItem> productList;
     private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
     private RecyclerView.LayoutManager layoutManager;
@@ -18,12 +19,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayList<ProductItem> exampleList = new ArrayList<>();
-        exampleList.add(new ProductItem(R.drawable.ic_product_a, "Line 1", "Line 2"));
-        exampleList.add(new ProductItem(R.drawable.ic_product_b, "Line 3", "Line 4"));
-        exampleList.add(new ProductItem(R.drawable.ic_product_c, "Line 5", "Line 6"));
-        exampleList.add(new ProductItem(R.drawable.ic_product_a, "Line 7", "Line 8"));
-        exampleList.add(new ProductItem(R.drawable.ic_product_b, "Line 9", "Line 10"));
-        exampleList.add(new ProductItem(R.drawable.ic_product_c, "Line 11", "Line 12"));
+        productList = new ArrayList<>();
+        productList.add(new ProductItem(R.drawable.ic_product_a, "Line 1", "Line 2"));
+        productList.add(new ProductItem(R.drawable.ic_product_b, "Line 3", "Line 4"));
+        productList.add(new ProductItem(R.drawable.ic_product_c, "Line 5", "Line 6"));
+        productList.add(new ProductItem(R.drawable.ic_product_a, "Line 7", "Line 8"));
+        productList.add(new ProductItem(R.drawable.ic_product_b, "Line 9", "Line 10"));
+        productList.add(new ProductItem(R.drawable.ic_product_c, "Line 11", "Line 12"));
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        adapter = new ProductAdapter(productList);
+
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
     }
 }
